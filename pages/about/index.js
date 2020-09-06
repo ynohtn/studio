696cc0d2d6e4087e202a1637a76634c0b8b4f6dc
ynohtn <anthonyrnd69@gmail.com>
2020-09-06 14:05:43 +0200

    (dev) : some fine tunnig

--- pages/about/index.js
+++ pages/about/index.js
@@ -12,46 +12,40 @@ export default function About({ res, slices }) {
   // console.log(slices)
 
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-    >
-      <Layout>
-        <Head>
-          {/*Primary Meta Tags*/}
-          <title>About us</title>
-          <meta name="title" content="About us at Studio" />
-          <meta name="description" content="Discover who we work with and what we love to do to your sound, finest audio engineering in Studio" />
+    <Layout>
+      <Head>
+        {/*Primary Meta Tags*/}
+        <title>About us</title>
+        <meta name="title" content="About us at Studio" />
+        <meta name="description" content="Discover who we work with and what we love to do to your sound, finest audio engineering in Studio" />
 
-          {/* Open Graph / Facebook */}
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://studio-seven.vercel.app/about" />
-          <meta property="og:title" content="About us at Studio" />
-          <meta property="og:description" content="Discover who we are, what we love, what we do. Finest audio engineers in Studio" />
-          <meta property="og:image" content="/meta-cover.jpg" />
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://studio-seven.vercel.app/about" />
+        <meta property="og:title" content="About us at Studio" />
+        <meta property="og:description" content="Discover who we are, what we love, what we do. Finest audio engineers in Studio" />
+        <meta property="og:image" content="/meta-cover.jpg" />
 
-          {/* Twitter */}
-          <meta property="twitter:card" content="/meta-cover.jpg" />
-          <meta property="twitter:url" content="https://studio-seven.vercel.app/about" />
-          <meta property="twitter:title" content="About us at Studio" />
-          <meta property="twitter:description" content="Discover who we are, what we love, what we do. Finest audio engineers in Studio" />
-          <meta property="twitter:image" content="/meta-cover.jpg" />
-        </Head>
+        {/* Twitter */}
+        <meta property="twitter:card" content="/meta-cover.jpg" />
+        <meta property="twitter:url" content="https://studio-seven.vercel.app/about" />
+        <meta property="twitter:title" content="About us at Studio" />
+        <meta property="twitter:description" content="Discover who we are, what we love, what we do. Finest audio engineers in Studio" />
+        <meta property="twitter:image" content="/meta-cover.jpg" />
+      </Head>
 
-        <h1 className={styles.abouttitle}>About us</h1>
+      <h1 className={styles.abouttitle}>About us</h1>
 
-        <section className={styles.aboutbody}>
-          <SliceZone sliceZone={slices} />
-        </section>
-        
-        <h2 className={styles.backlink}>
-          <NextLink href='/'>
-            <a>Back to Home</a>
-          </NextLink>
-        </h2>
-      </Layout>
-    </motion.div>
+      <section className={styles.aboutbody}>
+        <SliceZone sliceZone={slices} />
+      </section>
+      
+      <h2 className={styles.backlink}>
+        <NextLink href='/'>
+          <a>Back to Home</a>
+        </NextLink>
+      </h2>
+    </Layout>
   )
 }
 
--- pages/samples/index.js
+++ pages/samples/index.js
@@ -1,8 +1,6 @@
 import { Client } from '../../lib/prismic-configuration'
 import { useEffect } from 'react'
 import { gsap } from 'gsap'
-import { motion } from 'framer-motion'
-import { stagger, fadeInUp } from '../../lib/animation'
 import { getSamplePackData } from '../../lib/api'
 import styles from '../../styles/Samples.module.scss'
 
@@ -31,59 +29,51 @@ export default function Samples({ res, items }) {
   }, [])
 
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-    >
-      <Layout>
-        <Head>
+    <Layout isGrid={true}>
+      <Head>
 
-          {/*Primary Meta Tags*/}
-          <title>Samples</title>
-          <meta name="title" content="Samples from Studio"/>
-          <meta name="description" content="Discover sample packs made with love by passionnate musicians and audio engineers in Studio"/>
+        {/*Primary Meta Tags*/}
+        <title>Samples</title>
+        <meta name="title" content="Samples from Studio"/>
+        <meta name="description" content="Discover sample packs made with love by passionnate musicians and audio engineers in Studio"/>
 
-          {/* Open Graph / Facebook */}
-          <meta property="og:type" content="website"/>
-          <meta property="og:url" content="https://studio-seven.vercel.app/samples"/>
-          <meta property="og:title" content="Samples from Studio"/>
-          <meta property="og:description" content="Discover sample packs made with love by passionnate musicians and audio engineers in Studio"/>
-          <meta property="og:image" content="/meta-cover.jpg"/>
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://studio-seven.vercel.app/samples"/>
+        <meta property="og:title" content="Samples from Studio"/>
+        <meta property="og:description" content="Discover sample packs made with love by passionnate musicians and audio engineers in Studio"/>
+        <meta property="og:image" content="/meta-cover.jpg"/>
 
-          {/* Twitter */}
-          <meta property="twitter:card" content="/meta-cover.jpg"/>
-          <meta property="twitter:url" content="https://studio-seven.vercel.app/samples"/>
-          <meta property="twitter:title" content="Samples from Studio"/>
-          <meta property="twitter:description" content="Discover sample packs made with love by passionnate musicians and audio engineers in Studio"/>
-          <meta property="twitter:image" content="/meta-cover.jpg"/>
+        {/* Twitter */}
+        <meta property="twitter:card" content="/meta-cover.jpg"/>
+        <meta property="twitter:url" content="https://studio-seven.vercel.app/samples"/>
+        <meta property="twitter:title" content="Samples from Studio"/>
+        <meta property="twitter:description" content="Discover sample packs made with love by passionnate musicians and audio engineers in Studio"/>
+        <meta property="twitter:image" content="/meta-cover.jpg"/>
 
-        </Head>
+      </Head>
 
-        <h1>{res.data.samples_title[0].text}</h1>
-
-        <motion.div variants={stagger} className={styles.grid}>
-          {items.map((item, i) => (
-            <motion.div key={i} id={`griditem`} variants={fadeInUp}>
-              <NextLink href={'samples/[uid]'} as={`samples/${item.sample.uid}`}>
-                <a>
-                  <SmpCard
-                    name={item.sample.pack_name[0].text}
-                    cover={item.sample.pack_cover}
-                  />
-                </a>
-              </NextLink>
-            </motion.div>
-          ))}
-        </motion.div>
-        
-        <h2 className={styles.backlink}>
-          <NextLink href='/'>
-            <a>Back to Home</a>
+      <h1>{res.data.samples_title[0].text}</h1>
+      
+      <div className={styles.grid}>
+        {items.map((item, i) => (
+          <NextLink key={i} href={'samples/[uid]'} as={`samples/${item.sample.uid}`}>
+            <a id={`griditem`}>
+              <SmpCard
+                name={item.sample.pack_name[0].text}
+                cover={item.sample.pack_cover}
+              />
+            </a>
           </NextLink>
-        </h2>
-      </Layout>
-    </motion.div>
+        ))}
+      </div>
+
+      <h2 className={styles.backlink}>
+        <NextLink href='/'>
+          <a>Back to Home</a>
+        </NextLink>
+      </h2>
+    </Layout>
   )
 }
 
